67d8fd935c052b8b410b1feb4c4c0dbfcd69e8ae
ElDubsNZ <github@eldubs.com>
2025-01-30 14:26:54 +1300

    Initial commit

---
+++ 0. Scripts/MoveFilesDown.ps1
@@ -0,0 +1,40 @@
+﻿# The following resolves a system issue where filepaths can only be 260 characters long, so if your filepath is quite long, this will resolve the issue by allowing filepaths to be longer.
+# This can be removed if you're okay with manually fixing any that fail to move.
+[System.AppContext]::SetSwitch('Switch.System.IO.UseLegacyPathHandling', $false)
+[System.AppContext]::SetSwitch('Switch.System.IO.BlockLongPaths', $false)
+[System.Type]::GetType('System.AppContextSwitches').GetField('_useLegacyPathHandling', [System.Reflection.BindingFlags]::Static -bor [System.Reflection.BindingFlags]::NonPublic).SetValue($null, 0)
+[System.Type]::GetType('System.AppContextSwitches').GetField('_blockLongPaths', [System.Reflection.BindingFlags]::Static -bor [System.Reflection.BindingFlags]::NonPublic).SetValue($null, 0)
+
+# Define the root directory to start the search
+$rootDirectory = "\\dubNAS\Documents\Big Finish Test\Big Finish\1. Doctor Who"
+
+# Get all files in the root directory and its subdirectories
+$files = Get-ChildItem -Path $rootDirectory -Recurse -File
+
+foreach ($file in $files) {
+    # Check if the file name ends with an ellipsis and adjust the folder name accordingly
+    $folderName = if ($file.BaseName -match "\.\.\.$") {
+        # Remove the ellipsis for the folder name
+        $file.BaseName.Substring(0, $file.BaseName.Length - 3)
+    } else {
+        $file.BaseName
+    }
+    # Same but for a two point elipses.
+    $folderName = if ($file.BaseName -match "\.\.$") {
+        # Remove the ellipsis for the folder name
+        $file.BaseName.Substring(0, $file.BaseName.Length - 3)
+    } else {
+        $file.BaseName
+    }
+
+    # Create a new directory path
+    $newDirectory = Join-Path $file.Directory.FullName $folderName
+    
+    # Create the directory if it doesn't exist
+    if (-not (Test-Path -Path $newDirectory)) {
+        New-Item -ItemType Directory -Path $newDirectory
+    }
+
+    # Move the file into the new directory
+    Move-Item -Path $file.FullName -Destination $newDirectory
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Big Finish Metadata
+
+## Overview
+
+This repository is aimed at collectors of Big Finish Audios who would like a consistent and convenient folder structure with which to store all your audios.
+
+I created this intending on using it with audiobookshelf, a self-hosted audiobook player, however it should work with anything that supports opf files.
+
+## Corrections
+
+The source folder structure was copied from another collector, it may have errors, I will update this repository as time goes on with a "corrections" powershell script that will automatically rename files from how they were originally named to the correct name where needed.
+
+Please log any corrections needed under issues.
+
+## Cover Art
+
+I would love to include cover art, but I'm not certain of the copyright status and don't want to risk github taking the repository down, so you'll need to source this yourself.
+
+## Folder Structure
+
+The collector I got the folder structure from kept audio files for a full season in the same folder, however audiobookshelf requires these to be separate.
+
+Another issue is Windows has issues with file path lengths over 260 characters.
+
+So I've created a script that fixes this, and shifts all files within a directory (and all nested sub-directories) into their own folders.
+
+If you need this, run the "MoveFilesDown" script in the Scripts folder.
+
+**IMPORTANT**: Run this *before* you copy across the OPF files otherwise they'll get placed in an even further sub-directory.
